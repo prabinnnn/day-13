2a19ce220fe4f88e987fe7e149520b1bfb0c1cee
prabinnnn <prabinbhandari120@gmail.com>
2024-02-04 16:03:14 +0545

    add index

--- module/libaray/hotel/hotel.route.js
+++ module/libaray/hotel/hotel.route.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const e = require("express");
 const hotelcontroller = require("./hotel.controller");
 const checkrole = () => {
   return (req, res, next) => {
