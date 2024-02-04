b40025a54a8acd8e56de8851a92598fce479e9f8
prabinnnn <prabinbhandari120@gmail.com>
2024-02-04 13:12:48 +0545

    add index

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,4 @@ const bookrouter = require("../module/libaray/libaray.route");
 const hotelutils = require("../utils/totalday");
 router.use("/api/v1", bookrouter);
 
-(module.exports = router), hotelutils;
+module.exports = { router, hotelutils };
